14fdbdc1f5985a9c3ee7d8563b8d578c12429a39
JAlcocerT <contact@jalcocertech.xyz>
2025-03-02 01:37:27 +0100

    fixed base path

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -6,7 +6,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celestialdocs.hyperoot.dev",
+  site: "https://jalcocert.github.io/",
+  base: '/mechanism', //as per the repo name
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
